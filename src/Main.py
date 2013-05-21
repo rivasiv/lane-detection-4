d017b711e5996e9017861ec6d1063d01eefcb616
Rafael <rafaellopezgtz@gmail.com>
2013-05-21 06:58:52 -0400

    update main

--- src/Main.py
+++ src/Main.py
@@ -1,9 +1,11 @@
 import cv2 as cv
-import points as dr
+from LaneDetection import LaneDetection
+from LaneMarkersModel import normalize
 import numpy as np
+from marcador import marcador
+from Line import Line
 import time
-from orientation import orientation
-from lanedetection import LaneDetection
+
 
 
 log = open("log.txt","w")
@@ -12,12 +14,19 @@ log.close()
 CAN_L = 70
 CAN_R = 170
 #Archivo de entrada para pruebas
-File_Test = "video5.mp4"
+#Prueba1
+#File_Test = "video5.mp4"
+#Prueba2
+File_Test = "009-1.mp4"
 #Captura de camara en tiempo real
 REALTIME = False
 DEVICE = 0
-UMBRAL_H = 200
-UMBRAL_L = 255
+UMBRAL_H = 255
+UMBRAL_L = 200
+
+AI = [0, 124, 637, 298]
+sensorsNumber = 50
+sensorsWidth = 70
 #Iniciando lectura de frames
 if REALTIME == True:
     capture = cv.VideoCapture(DEVICE) #6 7 8
@@ -30,11 +39,11 @@ if capture.isOpened() == False:
     print "Error al Abrir el Archivo de entrada"
     exit()
 
+#Iniciando copia del video
+videoWriter = cv.VideoWriter('record.avi', cv.cv.CV_FOURCC('M','J','P','G'), 30, (640, 480), 1)
 
 #Parametros de la imagen 
-cropArea = [0, 124, 637, 298]
-sensorsNumber = 50
-sensorsWidth = 70
+
 
 #inicio y final de las lineas
 line1LStart = np.array([35, 128])
@@ -43,17 +52,17 @@ line1RStart = np.array([632, 146])
 line1REnd = np.array([476, 11])
 
 #Obtener el primer frame para el modelo de color
-flag, imgFull = capture.read()
-img = imgFull[cropArea[1]:cropArea[3], cropArea[0]:cropArea[2]]
+flag, resize = capture.read()
+img = resize[AI[1]:AI[3], AI[0]:AI[2]]
 
 #Initialize left lane
 leftLineColorModel = LaneMarkersModel()
-leftLine = LineDetector(cropArea, sensorsNumber, sensorsWidth, line1LStart, line1LEnd, leftLineColorModel)
+leftLine = LineDetector(AI, sensorsNumber, sensorsWidth, line1LStart, line1LEnd, leftLineColorModel)
 
 #Initialize right lane
 rightLineColorModel = LaneMarkersModel()
 #rightLineColorModel.InitializeFromImage(np.float32(img)/255.0, "Select right line")
-rightLine = LineDetector(cropArea, sensorsNumber, sensorsWidth, line1RStart, line1REnd, rightLineColorModel)
+rightLine = LineDetector(AI, sensorsNumber, sensorsWidth, line1RStart, line1REnd, rightLineColorModel)
 
 Frame_ = 0
 fr = 0
@@ -65,7 +74,7 @@ while(cv.waitKey(1) != 27):
     #print Frame_
     #Leer y cortar en esta parte del procesos se corta el horizonte y algunas partes
     #que puedan proporcionar ruido para la imagen
-    flag, imgFull = capture.read()
+    flag, resize = capture.read()
     if flag == False: break 
 
     #Pre-procesamiento
@@ -75,7 +84,7 @@ while(cv.waitKey(1) != 27):
     #cv.Threshold(grayscale,grayscale,80, 255, cv.CV_THRESH_BINARY | cv.CV_THRESH_OTSU);
     grayImage = cv.cvtColor(np.uint8(img*255), cv.COLOR_RGB2GRAY)
     img = cv.threshold(grayImage,UMBRAL_H,UMBRAL_L,cv.THRESH_BINARY)
-    img = np.float32(imgFull[cropArea[1]:cropArea[3], cropArea[0]:cropArea[2]])/255.0
+    img = np.float32(resize[AI[1]:AI[3], AI[0]:AI[2]])/255.0
     #para convertir la imagen a HSV
     hsv = np.float32(cv.cvtColor(img, cv.COLOR_RGB2HSV))
     #Pasando la imagen a filtros de canny
@@ -84,11 +93,19 @@ while(cv.waitKey(1) != 27):
     #grayImage = cv2.cvtColor(sourceImage, cv2.COLOR_RGB2GRAY)
     #fil = cv.threshold(cv.cvtColor(np.uint8(img*255)), cv.COLOR_RGB2GRAY,UMBRAL_H,UMBRAL_L,cv.THRESH_BINARY)
     #canny = cv.Canny(cv.cvtColor(np.uint8(img*255), cv.COLOR_RGB2GRAY),CAN_L,CAN_R)
-    canny = cv.Canny(cv.cvtColor(np.uint8(img*255), cv.COLOR_RGB2GRAY),90,90*3)
+    canny = cv.Canny(cv.cvtColor(np.uint8(img*255), cv.COLOR_RGB2GRAY),100,100*3)
  
     #Salida de las imagenes 
-    outputImg = img.copy()
-    post = imgFull.copy()
+    out = img.copy()
+    post = resize.copy()
+
+    #procesamiento de los frames con las funciones de deteccion de lineas
+
+    #Pre-procesamiento para la linea Izquierda
+    leftLine.LaneDetection(img, hsv, canny, out, post)
+
+    #Pre-procesamiento para la linea derecha
+    rightLine.LaneDetection(img, hsv, canny, out, post)
     
     #Mostrando la salida de las imagenes 
     cv.imshow("Output", canny)
